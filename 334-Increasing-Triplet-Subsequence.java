eaafcde846480a2da6ccf7b3dc20442efd2995eb
Md Raghib <kraghib123@gmail.com>
2026-08-25 22:35:17 +0530

    Refactor increasingTriplet method for clarity

--- 334-Increasing-Triplet-Subsequence.java
+++ 334-Increasing-Triplet-Subsequence.java
@@ -3,10 +3,10 @@ class Solution {
        int first = Integer.MAX_VALUE ;
        int second = Integer.MAX_VALUE ;
        int third = Integer.MAX_VALUE ;
-
+        
        for(int i=0;i<nums.length;i++){
-
            int ele= nums[i];
+           
            if(first>=ele){
                first = ele ;
            }else if ( second >= ele){
@@ -15,6 +15,7 @@ class Solution {
                third = ele ;
                return true ;
            }
+       
        }
        return false ;
     }
