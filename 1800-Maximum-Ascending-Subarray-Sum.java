5171493c976dc87a50098b3f44d16ada8589f97d
Md Raghib <kraghib123@gmail.com>
2026-08-25 22:34:40 +0530

    Fix maxAscendingSum method to handle new input cases

--- 1800-Maximum-Ascending-Subarray-Sum.java
+++ 1800-Maximum-Ascending-Subarray-Sum.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxAscendingSum(int[] nums) {
         int max = nums[0];
         int currmax = nums[0];
-
+        
         for(int i=1 ; i<nums.length;i++){
             if(nums[i-1]<nums[i]){
                 currmax = currmax + nums[i];
@@ -11,6 +11,7 @@ class Solution {
                 currmax = nums[i];
             }
         }
+        
          max = Math.max(max , currmax);
          return max ;
     }
