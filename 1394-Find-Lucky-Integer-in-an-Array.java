433275759cfb3daff50baf5bd87603bf137aa11b
Md Raghib <kraghib123@gmail.com>
2026-08-25 22:34:08 +0530

    Refactor findLucky method for clarity

--- 1394-Find-Lucky-Integer-in-an-Array.java
+++ 1394-Find-Lucky-Integer-in-an-Array.java
@@ -1,16 +1,9 @@
 class Solution {
     public static int findLucky(int[] numbers) {  
-        
         int[] freq = new int[501];             
-        
-
         for (int num : numbers) freq[num]++;   
-        
-
         for (int i = 500; i > 0; i--) 
             if (i == freq[i]) return i;     
-
-        return -1;                             
-         
+        return -1;                                  
     }
 }
